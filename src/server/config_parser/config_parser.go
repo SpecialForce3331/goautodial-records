dc82f5f90d2c7e81a89c241674b67bdd8a281f81
Sizov Sergey <sizovIT@gmail.com>
2017-08-10 17:16:48 +0500

    Added days count to select into config and some code to work it

--- src/server/config_parser/config_parser.go
+++ src/server/config_parser/config_parser.go
@@ -1,26 +1,27 @@
 package config_parser
 
 import (
-	"os"
 	"bufio"
 	"log"
+	"os"
 	"strings"
 )
 
 const configName string = "config.txt"
 
 type Config struct {
-	HTTP_PORT string
-	SQL_LOGIN string
-	SQL_PASSWORD string
-	SQL_HOST string
-	SQL_PORT string
-	SQL_DB string
+	HTTP_PORT                 string
+	SQL_LOGIN                 string
+	SQL_PASSWORD              string
+	SQL_HOST                  string
+	SQL_PORT                  string
+	SQL_DB                    string
 	SQL_FIELD_INBOUND_list_id string
+	DAYS_SELECT_COUNT         string
 }
 
 func openFile() *os.File {
-	file,err := os.Open(configName)
+	file, err := os.Open(configName)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -36,6 +37,7 @@ func parseConfig(file *os.File) Config {
 	var SQL_PORT string
 	var SQL_DB string
 	var SQL_FIELD_INBOUND_list_id string
+	var DAYS_SELECT_COUNT string
 
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
@@ -43,30 +45,32 @@ func parseConfig(file *os.File) Config {
 		lineArguments := strings.Split(line, "=")
 
 		switch key := lineArguments[0]; key {
-			case "HTTP_PORT":
-				HTTP_PORT = lineArguments[1]
-			case "SQL_LOGIN":
-				SQL_LOGIN = lineArguments[1]
-			case "SQL_PASSWORD":
-				SQL_PASSWORD = lineArguments [1]
-			case "SQL_HOST":
-				SQL_HOST = lineArguments[1]
-			case "SQL_PORT":
-				SQL_PORT = lineArguments[1]
-			case "SQL_DB":
-				SQL_DB = lineArguments[1]
-			case "SQL_FIELD_INBOUND_list_id":
-				SQL_FIELD_INBOUND_list_id = lineArguments[1]
-			default:
-				log.Fatal("Config error! Please check config.txt file!")
+		case "HTTP_PORT":
+			HTTP_PORT = lineArguments[1]
+		case "SQL_LOGIN":
+			SQL_LOGIN = lineArguments[1]
+		case "SQL_PASSWORD":
+			SQL_PASSWORD = lineArguments[1]
+		case "SQL_HOST":
+			SQL_HOST = lineArguments[1]
+		case "SQL_PORT":
+			SQL_PORT = lineArguments[1]
+		case "SQL_DB":
+			SQL_DB = lineArguments[1]
+		case "SQL_FIELD_INBOUND_list_id":
+			SQL_FIELD_INBOUND_list_id = lineArguments[1]
+		case "DAYS_SELECT_COUNT":
+			DAYS_SELECT_COUNT = lineArguments[1]
+		default:
+			log.Fatal("Config error! Please check config.txt file!")
 		}
 	}
 
-	if HTTP_PORT == "" || SQL_LOGIN == "" || SQL_PASSWORD == "" || SQL_HOST == "" || SQL_PORT == "" || SQL_DB == "" || SQL_LOGIN == "" || SQL_FIELD_INBOUND_list_id == "" {
+	if HTTP_PORT == "" || SQL_LOGIN == "" || SQL_PASSWORD == "" || SQL_HOST == "" || SQL_PORT == "" || SQL_DB == "" || SQL_LOGIN == "" || SQL_FIELD_INBOUND_list_id == "" || DAYS_SELECT_COUNT == "" {
 		log.Fatal("Config error! One of fields are null")
 	}
 
-	return Config{HTTP_PORT, SQL_LOGIN, SQL_PASSWORD, SQL_HOST, SQL_PORT, SQL_DB, SQL_FIELD_INBOUND_list_id}
+	return Config{HTTP_PORT, SQL_LOGIN, SQL_PASSWORD, SQL_HOST, SQL_PORT, SQL_DB, SQL_FIELD_INBOUND_list_id, DAYS_SELECT_COUNT}
 
 }
 
--- src/server/server.go
+++ src/server/server.go
@@ -1,21 +1,21 @@
 package main
 
 import (
+	"database/sql"
+	"encoding/json"
 	"fmt"
+	_ "github.com/go-sql-driver/mysql"
 	"log"
 	"net/http"
-	"database/sql"
-	_ "github.com/go-sql-driver/mysql"
-	"encoding/json"
 	"server/config_parser"
 )
 
 type Record struct {
-    Agent string
-    Phone string
-    Location string
-    CallDate string
-    IsInbound bool
+	Agent     string
+	Phone     string
+	Location  string
+	CallDate  string
+	IsInbound bool
 }
 
 var cfg config_parser.Config
@@ -29,19 +29,26 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	w.Header().Set("Content-Type", "application/json")
-  	w.Write(records_json)
+	w.Write(records_json)
 }
 
 func dbOpen() *sql.DB {
-	db, err := sql.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s:%s)/%s",cfg.SQL_LOGIN, cfg.SQL_PASSWORD, cfg.SQL_HOST, cfg.SQL_PORT, cfg.SQL_DB))
+	db, err := sql.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", cfg.SQL_LOGIN, cfg.SQL_PASSWORD, cfg.SQL_HOST, cfg.SQL_PORT, cfg.SQL_DB))
 	if err != nil {
-    	panic(err.Error()) // Just for example purpose. You should use proper error handling instead of panic
+		panic(err.Error()) // Just for example purpose. You should use proper error handling instead of panic
 	}
 	return db
 }
 
 func dbSelect(db *sql.DB) []Record {
-	rows, err := db.Query("SELECT agent, phone, location, call_date, list_id FROM goautodial_recordings_views WHERE location IS NOT NULL AND phone IS NOT NULL and list_id IS NOT NULL ORDER BY call_date DESC")
+	rows, err := db.Query(fmt.Sprintf(`
+		SELECT agent, phone, location, call_date, list_id 
+		FROM goautodial_recordings_views 
+		WHERE location IS NOT NULL 
+		AND phone IS NOT NULL 
+		AND list_id IS NOT NULL 
+		AND call_date BETWEEN DATE_SUB(CURDATE(), INTERVAL %s DAY) AND NOW() 
+		ORDER BY call_date DESC`, cfg.DAYS_SELECT_COUNT))
 
 	if err != nil {
 		panic(err.Error()) // proper error handling instead of panic in your app
@@ -52,36 +59,30 @@ func dbSelect(db *sql.DB) []Record {
 	var records []Record
 
 	for rows.Next() {
-	        var agent string
-			var phone string
-			var location string
-			var callDate string
-			var listId string
-			var isInbound bool = false
+		var agent string
+		var phone string
+		var location string
+		var callDate string
+		var listId string
+		var isInbound bool = false
 
-	        if err := rows.Scan(&agent, &phone, &location, &callDate, &listId); err != nil {
-                log.Println(err)
-                log.Println(agent, phone, location, callDate, listId)
-	        }
+		if err := rows.Scan(&agent, &phone, &location, &callDate, &listId); err != nil {
+			log.Println(err)
+			log.Println(agent, phone, location, callDate, listId)
+		}
 
-	        // fmt.Println(listId, listId == "900")
+		if listId == cfg.SQL_FIELD_INBOUND_list_id {
+			isInbound = true
+		}
 
-	        if listId == cfg.SQL_FIELD_INBOUND_list_id {
-	        	isInbound = true
-	        }
+		record := Record{agent, phone, location, callDate, isInbound}
 
-	        record := Record{agent, phone, location, callDate, isInbound}
-
-	        records = append(records, record)
-
-	        
+		records = append(records, record)
 	}
 
 	if err := rows.Err(); err != nil {
-	        log.Fatal(err)
+		log.Fatal(err)
 	}
-
-	// fmt.Println(records)
 	return records
 }
 
@@ -93,10 +94,10 @@ func getRecords() []Record {
 }
 
 func main() {
-	cfg = config_parser.GetConfig() 
+	cfg = config_parser.GetConfig()
 
-	fmt.Println(fmt.Sprintf("Server started!\nListening %s port...",cfg.HTTP_PORT))
+	fmt.Println(fmt.Sprintf("Server started!\nListening %s port...", cfg.HTTP_PORT))
 	http.HandleFunc("/records", handler)
 	http.Handle("/", http.FileServer(http.Dir("./web")))
-	http.ListenAndServe(fmt.Sprintf(":%s",cfg.HTTP_PORT), nil)
+	http.ListenAndServe(fmt.Sprintf(":%s", cfg.HTTP_PORT), nil)
 }
